fc15bb5d6d2be24f9fcda6571c7efa81f110681a
Beluszky Aranyköpései <192024038+beluszky-aranykopesei@users.noreply.github.com>
2025-07-19 15:45:18 +0200

    Update BeluszkyAI.js

--- scripts/BeluszkyAI.js
+++ scripts/BeluszkyAI.js
@@ -1,3 +1,7 @@
 function openBeluszkyAI() {
     window.open('https://bit.ly/beluszky-ai', '_blank');
 };
+
+function openOldVersion() {
+    window.open('https://beluszky-aranykopesei-stabil.vercel.app/', '_blank');
+};
