c9a6d981f0b216d7e1c42255bfea3c1e7a0cc843
Beluszky Aranyköpései <192024038+beluszky-aranykopesei@users.noreply.github.com>
2025-07-21 17:29:05 +0200

    Update Masolas.js

--- scripts/Masolas.js
+++ scripts/Masolas.js
@@ -1,6 +1,6 @@
 function copyToClipboard(element) {
     const text = element.parentElement.innerText;
-    const additionalText = "- Beluszky";
+    const additionalText = " - Beluszky";
     const fullText = text + additionalText;
     navigator.clipboard.writeText(fullText).then(() => {
         alert('Szöveg kimásolva: ' + fullText);
