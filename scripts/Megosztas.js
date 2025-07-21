022c2e1ffa1a8d194a988e3d5710b35db2dbf771
Beluszky Aranyköpései <192024038+beluszky-aranykopesei@users.noreply.github.com>
2025-07-21 18:04:49 +0200

    Create Megosztas.js

---
+++ scripts/Megosztas.js
@@ -0,0 +1,28 @@
+function showShareIcon(liElement, textToShare) {
+    // Ellenőrizzük, van-e már megosztás ikon
+    if (liElement.querySelector('.share-icon')) return;
+
+    const shareIcon = document.createElement('img');
+    shareIcon.src = 'Megosztas.png';
+    shareIcon.alt = 'Megosztás ikon';
+    shareIcon.title = 'Megosztás';
+    shareIcon.className = 'share-icon';
+    shareIcon.style.width = '26px';
+    shareIcon.style.height = '26px';
+    shareIcon.style.marginLeft = '8px';
+    shareIcon.style.cursor = 'pointer';
+
+    shareIcon.onclick = () => {
+        if (navigator.share) {
+            navigator.share({
+                title: 'Beluszky Aranyköpés',
+                text: textToShare,
+                url: location.href
+            });
+        } else {
+            alert('Megosztás nem támogatott ezen az eszközön.');
+        }
+    };
+
+    liElement.appendChild(shareIcon);
+}
