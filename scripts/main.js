b51f8952d37d03b3dbb1d429a908df88653180da
Beluszky Aranyköpései <192024038+beluszky-aranykopesei@users.noreply.github.com>
2025-10-21 21:30:35 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 import { showPopup } from './Nagyitas.js';
 import { copyToClipboard } from './Masolas.js';
-import { getDailyQuote } from './NapiAranykopes.js';
 
 const supabaseUrl = 'https://wqerrkvnnwdyphnbvjrn.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxZXJya3ZubndkeXBobmJ2anJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0MTI5NTgsImV4cCI6MjA2MDk4ODk1OH0.rSQfUSGCzbn4bySlnCaAjFDhgTsFys8y_fA57ntZ1DI'; // public kulcs a Supabase Settingsben
@@ -86,3 +85,4 @@ document.addEventListener('DOMContentLoaded', () => {
     quoteElement.innerText = quote;
   }
 });
+
