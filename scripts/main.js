1fdc7f738330a0985b11e817fb2f44872fabd540
Beluszky Aranyköpései <192024038+beluszky-aranykopesei@users.noreply.github.com>
2025-12-09 21:20:21 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2';
 import { showPopup } from './Nagyitas.js';
 import { copyToClipboard } from './Masolas.js';
 
@@ -77,3 +77,4 @@ data.forEach(row => {
   quoteList.appendChild(li);
 });
 });
+
